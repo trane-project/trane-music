4c4a3dcc6358a9d031cd7c326edac39fdf5730e5
Martin Martinez Rivera <mrtnz.rvr@gmail.com>
2022-11-27 20:41:53 -0800

    Remove dependency of minor scale course on major scale course

--- src/theory/minor_scale.rs
+++ src/theory/minor_scale.rs
@@ -3,7 +3,7 @@ use lazy_static::lazy_static;
 use trane::course_builder::{music::scales::ScaleType, CourseBuilder};
 use ustr::Ustr;
 
-use crate::theory::{major_scale, scale_course::ScaleCourse};
+use crate::theory::scale_course::ScaleCourse;
 
 lazy_static! {
     pub static ref COURSE_ID: Ustr = Ustr::from("trane::music::theory::minor_scale");
@@ -12,7 +12,7 @@ lazy_static! {
 pub fn course_builder() -> Result<CourseBuilder> {
     let scale_course = ScaleCourse {
         course_id: *COURSE_ID,
-        dependencies: vec![*major_scale::COURSE_ID],
+        dependencies: vec![],
         directory_name: "minor_scale".to_string(),
         scale: ScaleType::Minor,
         note_alias: Some(|note| note.relative_minor()),
